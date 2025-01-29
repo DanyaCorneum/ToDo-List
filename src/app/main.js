1a5b49746b5e99b5acb4c1574a6f68efaf4df2bb
Ratkov_Daniil <daniilratkov3@gmail.com>
2025-01-29 15:47:28 +0700

    added new func for main.js and styles for style.css

--- src/app/main.js
+++ src/app/main.js
@@ -1,15 +1,37 @@
 //global
+const todoList = document.querySelector(".todo-list");
 let todos = [];
 let users = [];
 
 //atach events
 document.addEventListener("DOMContentLoaded", initApp);
 //DOM logic
-
+function getUser(userid) {
+  console.log(userid);
+  const user = users.find((u) => u.id === userid);
+  return user.name;
+}
+function printTodo({ completed, id, title, userId }) {
+  const li = document.createElement("li");
+  li.classList.add("todo-item");
+  li.dataset.id = id;
+  li.innerHTML = `<span>${title} by ${getUser(userId)}</span>`;
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = completed;
+  const close = document.createElement("span");
+  close.innerHTML = "&times;";
+  close.classList.add("close");
+  li.prepend(checkbox);
+  li.append(close);
+  todoList.prepend(li);
+}
 //event logic
 function initApp() {
   Promise.all([getAllTodos(), getAllUsers()]).then((values) => {
     [todos, users] = values;
+    console.log(todos);
+    todos.forEach((el) => printTodo(el));
   });
 }
 
