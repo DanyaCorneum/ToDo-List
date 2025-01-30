6a42f4abf2bb4888065890db00baa6e64747698f
Ratkov_Daniil <daniilratkov3@gmail.com>
2025-01-30 12:03:25 +0700

    added func for form

--- src/app/main.js
+++ src/app/main.js
@@ -1,11 +1,13 @@
 //global
 const todoList = document.querySelector(".todo-list");
 const userSelect = document.querySelector("#user-todo");
+const form = document.querySelector("#form");
 let todos = [];
 let users = [];
 
 //atach events
 document.addEventListener("DOMContentLoaded", initApp);
+form.addEventListener("submit", handleSubmit);
 //DOM logic
 function createUserOption(user) {
   const option = document.createElement("option");
@@ -15,7 +17,6 @@ function createUserOption(user) {
 }
 
 function getUser(userid) {
-  console.log(userid);
   const user = users.find((u) => u.id === userid);
   return user.name;
 }
@@ -35,10 +36,18 @@ function printTodo({ completed, id, title, userId }) {
   todoList.prepend(li);
 }
 //event logic
+function handleSubmit(event) {
+  event.preventDefault();
+  createTodo({
+    userId: Number(form.user.value),
+    title: form.todo.value,
+    completed: false,
+  });
+}
+
 function initApp() {
   Promise.all([getAllTodos(), getAllUsers()]).then((values) => {
     [todos, users] = values;
-    console.log(todos);
     todos.forEach((el) => printTodo(el));
     users.forEach((user) => createUserOption(user));
   });
@@ -55,4 +64,17 @@ async function getAllUsers() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await response.json();
   return data;
+}
+
+async function createTodo(todo) {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos", {
+    method: "POST",
+    body: JSON.stringify(todo),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const todoContent = await response.json();
+  console.log(todo);
+  printTodo(todoContent);
 }
